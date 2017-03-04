20b669d127308577a57c2aef782ef38f7fbfb9df
jflute <dbflute@gmail.com>
2017-03-04 14:39:29 +0900

    DBFlute Engine: allcommonPrefix for e.g. DBFluteBeansJavaConfig

--- src/main/java/org/docksidestage/hangar/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
+++ src/main/java/org/docksidestage/hangar/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
@@ -63,7 +63,7 @@ public class VendorIdentityOnlyDbm extends AbstractDBMeta {
     // ===================================================================================
     //                                                                         Column Info
     //                                                                         ===========
-    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_FF05DA1C_415F_4388_BCBE_49D7BA665089", false, null, null, null, null, null, false);
+    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, Long.class, "identityOnlyId", null, true, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_4A263C21_F020_45AB_8E57_2CCAE1F37465", false, null, null, null, null, null, false);
 
     /**
      * IDENTITY_ONLY_ID: {PK, ID, NotNull, BIGINT(19)}
