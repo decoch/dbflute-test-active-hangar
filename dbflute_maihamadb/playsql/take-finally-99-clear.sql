5f5bc5953de7283c0e60e03bed051dd8e661caec
jflute <dbflute@gmail.com>
2017-03-26 01:46:21 +0900

    test tsv loading

--- dbflute_maihamadb/playsql/take-finally-99-clear.sql
+++ dbflute_maihamadb/playsql/take-finally-99-clear.sql
@@ -1,4 +1,4 @@
 
 -- delete records registered by TSV
-delete from MEMBER_STATUS where MEMBER_STATUS_CODE = 'SEA';
-delete from MEMBER_STATUS where MEMBER_STATUS_CODE = 'LAN';
+delete from MEMBER_STATUS where MEMBER_STATUS_CODE in ('SEA', 'LAN');
+delete from PRODUCT_STATUS where PRODUCT_STATUS_CODE in ('MYS', 'ONE', 'BBB');
